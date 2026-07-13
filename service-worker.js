e6554536bd043c2e65aba10373bddf7bc1e79e8a
mym1221 <mymcars.bh@gmail.com>
2026-07-13 22:27:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'site-bills-v2';
-const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
+const SHELL = ['./', './index.html', './manifest.json', './icon.png', './logo.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
